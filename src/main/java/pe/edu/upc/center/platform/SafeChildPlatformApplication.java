91762758d8a4640e5e3c404d78478c440adb0206
Lynn Meza <u20201c320@upc.edu.pe>
2024-10-28 21:30:49 -0500

    feat: backend project

---
+++ src/main/java/pe/edu/upc/center/platform/SafeChildPlatformApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.center.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeChildPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeChildPlatformApplication.class, args);
+	}
+
+}
